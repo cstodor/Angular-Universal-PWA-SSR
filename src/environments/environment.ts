94334bbb2227c6002ebeba1b72a807b1dd524368
Csaba Todor <csaba.todor@outlook.com>
2018-06-01 16:48:45 +0300

    Added Firebase Configuration Placeholders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    databaseURL: 'YOUR_DATABASE_URL',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGE_SENDER_ID'
+  }
 };
 
 /*
