dcda2aa4eb7d48d7f8f59742f7db4ee955a23ddd
Csaba Todor <csaba.todor@outlook.com>
2018-06-01 19:49:56 +0300

    Added Routes for fruits, animals. animal/:name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
+import { FruitsListComponent } from './fruits-list/fruits-list.component';
+import { AnimalsListComponent } from './animals-list/animals-list.component';
+import { AnimalDetailsComponent } from './animal-details/animal-details.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'fruit/:name', component: FruitDetailsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'fruits', component: FruitsListComponent },
+  { path: 'fruit/:name', component: FruitDetailsComponent },
+  { path: 'animals', component: AnimalsListComponent },
+  { path: 'animal/:name', component: AnimalDetailsComponent }
 ];
 
 @NgModule({
