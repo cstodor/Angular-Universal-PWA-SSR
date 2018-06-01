a6238b91f2257a3105a45ceb2c1cfd06ef369018
Csaba Todor <csaba.todor@outlook.com>
2018-06-01 16:15:59 +0300

    Imported and Declared Firestore Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// AngularFire2 Modules
+import { AngularFireModule, FirebaseOptionsToken } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitDetailsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserTransferStateModule, // For State Transfer
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: FirebaseOptionsToken, useValue: environment.firebase }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
