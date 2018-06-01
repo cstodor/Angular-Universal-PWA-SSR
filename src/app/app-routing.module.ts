9a7eff12487c4962638d75fcae9c146f0bfd43df
Csaba Todor <csaba.todor@outlook.com>
2018-06-01 16:47:41 +0300

    Added Routing for Fruit Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fruit/:name', component: FruitDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
