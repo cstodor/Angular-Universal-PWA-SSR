29ae050aca27bcdcf36d95df8326d595989cf89e
Csaba Todor <csaba.todor@outlook.com>
2018-06-03 21:05:33 +0300

    Reorganized Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Service Worker
+import { ServiceWorkerModule } from '@angular/service-worker';
+// Material Modules
+import { MatCardModule } from '@angular/material';
+// FLex Layout
+import { FlexLayoutModule } from '@angular/flex-layout';
 // AngularFire2 Modules
 import { AngularFireModule, FirebaseOptionsToken } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -7,24 +13,18 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-// Service Worker
-import { ServiceWorkerModule } from '@angular/service-worker';
-// Material Modules
-import { MatCardModule } from '@angular/material';
-// FLex Layout
-import { FlexLayoutModule } from '@angular/flex-layout';
 // Routing
 import { AppRoutingModule } from './app-routing.module';
 // Components
 import { AppComponent } from './app.component';
-import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
-import { FruitsListComponent } from './fruits-list/fruits-list.component';
+import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { PrimaryMenuComponent } from './shared/primary-menu/primary-menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FruitsListComponent } from './fruits-list/fruits-list.component';
+import { FruitDetailsComponent } from './fruit-details/fruit-details.component';
 import { AnimalsListComponent } from './animals-list/animals-list.component';
 import { AnimalDetailsComponent } from './animal-details/animal-details.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
