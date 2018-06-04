a551b06fc84ccded7618d8a19f0aba1b1bcf6848
Csaba Todor <csaba.todor@outlook.com>
2018-06-04 18:41:25 +0300

    imported FlexLayoutServerModule

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
-    ServerTransferStateModule, //  <-- needed for state transfer
-    ModuleMapLoaderModule // <-- needed for lazy-loaded routes
+    ServerTransferStateModule, // needed for state transfer
+    ModuleMapLoaderModule, // needed for lazy-loaded routes
+    FlexLayoutServerModule // needed for flax-layout to work on server
   ],
   bootstrap: [AppComponent],
 })
